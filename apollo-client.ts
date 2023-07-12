a55c59efd2f067decf5864c4ab49363e602384e3
sdelvalle57 <sdelvalle57@gmail.com>
2023-07-12 12:40:17 -0500

    with graphql

---
+++ apollo-client.ts
@@ -0,0 +1,9 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+
+const client = new ApolloClient({
+    uri: "http://localhost:7878/graphql",
+    cache: new InMemoryCache(),
+});
+
+export default client;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ApolloProvider } from '@apollo/client'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import client from '../../apollo-client';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
